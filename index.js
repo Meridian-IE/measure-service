9b8e3673c7510681e4512b3cac97845fdecf4c6d
Julian Gruber <julian@juliangruber.com>
2023-07-19 12:00:25 +0200

    refactor

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 import getRawBody from 'raw-body'
 import assert from 'http-assert'
+import { validate } from './lib/validate.js'
 
 //
 // Phase 1: Store the measurements
 //
-const validate = (obj, key, { type }) => {
-  assert(Object.keys(obj).includes(key) && obj[key] !== null, 400)
-  if (type === 'date') {
-    const date = new Date(obj[key])
-    assert(!isNaN(date.getTime()), 400)
-  } else {
-    assert.strictEqual(typeof obj[key], type, 400)
-  }
-}
-
 const handler = async (req, res, client) => {
   assert.strictEqual(req.url, '/measurements', 404)
   assert.strictEqual(req.method, 'POST', 404)
